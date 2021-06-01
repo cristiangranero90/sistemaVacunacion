8f031ed0f70dc8eb8f032d6d755abad520dee084
Cristian Granero <cristian_granero90@yahoo.com>
2021-06-01 07:48:58 -0300

    mandalo

--- 2021C1-TP_CentroVacunacion/src/centroVacunacion/Astrazeneca.java
+++ 2021C1-TP_CentroVacunacion/src/centroVacunacion/Astrazeneca.java
@@ -2,6 +2,8 @@ package centroVacunacion;
 
 public class Astrazeneca extends Vacuna {
 	
+	int numeroDePrueba;
+	
 	public Astrazeneca (String nombre, int cant, Fecha fi) {
 		super(nombre,cant,fi);
 	}
